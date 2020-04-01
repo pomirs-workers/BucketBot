01c8ea3bfffe742e0427cfe72604b138a14f8668
Vlad Ivanov <vivanov@metaprime.ru>
2020-04-01 22:51:32 +0300

    encs

--- motor.py
+++ motor.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 from lib.pi_pwm.module import PWM
+import time
 
+UPD_FREQ = 0.005  # in seconds
 
 class Motor:
 
@@ -18,24 +20,29 @@ class Motor:
         self.__gpio_init__()
         self.__apply__(0)
 
+        self.__angle_t__ = 0
+        self.__l_enc_a__ = 0
+
     def __gpio_init__(self):
-        GPIO.setup(self.__config__['pin_a'], GPIO.OUT)
-        GPIO.setup(self.__config__['pin_b'], GPIO.OUT)
+        GPIO.setup(self.__config__['m_a'], GPIO.OUT)
+        GPIO.setup(self.__config__['m_b'], GPIO.OUT)
+        GPIO.setup(self.__config__['en_a'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.__config__['en_b'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def __apply__(self, speed):
         self.__speed__ = speed
         if speed == 0:
             self.self.__pwm_ch__.set(0)
-            GPIO.output(self.__config__['pin_a'], False)
-            GPIO.output(self.__config__['pin_b'], False)
+            GPIO.output(self.__config__['m_a'], False)
+            GPIO.output(self.__config__['m_b'], False)
         elif speed > 0:
             self.self.__pwm_ch__.set(speed)
-            GPIO.output(self.__config__['pin_a'], True)
-            GPIO.output(self.__config__['pin_b'], False)
+            GPIO.output(self.__config__['m_a'], True)
+            GPIO.output(self.__config__['m_b'], False)
         else:
             self.self.__pwm_ch__.set(-1 * speed)
-            GPIO.output(self.__config__['pin_a'], False)
-            GPIO.output(self.__config__['pin_b'], True)
+            GPIO.output(self.__config__['m_a'], False)
+            GPIO.output(self.__config__['m_b'], True)
 
     def go(self, speed):
         if self.config['log']:
@@ -53,12 +60,21 @@ class Motor:
         self.__angle__ = 0
 
     def update(self):
+        time_mow = time.time()
         old_angle = self.__angle__
-        new_angle = 0  # TODO
-        self.__angle__ = new_angle
-        if self.config['log']:
-            print('[update] O/N/D = ' + str(old_angle) + '/' + str(new_angle) + '/' + str(new_angle - old_angle))
-        return new_angle - old_angle
+        if time_mow - self.__angle_t__ > UPD_FREQ:
+            enc_a = GPIO.input(self.__config__['enc_a'])
+            enc_b = GPIO.input(self.__config__['enc_b'])
+            if not enc_a and self.__l_enc_a__:
+                if enc_b:
+                    self.__angle__ += 1
+                else:
+                    self.__angle__ -= 1
+            self.__l_enc_a__ = enc_a
+            if self.config['log']:
+                print('[update] O/N/D = ' + str(old_angle) + '/' + str(self.__angle__) + '/' + str(self.__angle__ - old_angle))
+            return self.__angle__ - old_angle
+        return 0
 
     def get_angle(self):
         return self.__angle__
