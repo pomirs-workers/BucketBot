c7bc120ce7c948df006e9cabc0d1d00c4629fb8e
Vlad Ivanov <vivanov@metaprime.ru>
2020-04-01 21:47:22 +0300

    motors

---
+++ motor.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+from lib.pi_pwm.module import PWM
+
+class Motor:
+
+    @staticmethod
+    def apply_requirements():
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+
+    def __init__(self, config):
+        self.pin_a = config['pin_a']
+        self.pin_b = config['pin_b']
+        self.pin_pwm = config['pin_pwm']
+        self.pwm_channel = PWM(self.pin_pwm)
+        self.angle = 0.0
+        GPIO.setup(self.pin_a, GPIO.OUT)
+        GPIO.setup(self.pin_b, GPIO.OUT)
+        self.__apply__(0)
+
+    def __apply__(self, speed):
+        if speed == 0:
+            self.pwm_channel.set(0)
+            GPIO.output(self.pin_a, False)
+            GPIO.output(self.pin_b, False)
+        elif speed > 0:
+            self.pwm_channel.set(speed)
+            GPIO.output(self.pin_a, True)
+            GPIO.output(self.pin_b, False)
+        else:
+            self.pwm_channel.set(-1 * speed)
+            GPIO.output(self.pin_a, False)
+            GPIO.output(self.pin_b, True)
+
+    def go(self, speed):
+        self.__apply__(speed)
+
+    def stop(self):
+        self.go(0)
+
+    def reset(self):
+        self.angle = 0
+
+    def update(self):
+        old_angle = self.angle
+        new_angle = 0
+        self.angle = new_angle
+        return new_angle - old_angle
+
+    def get_angle(self):
+        return self.angle
